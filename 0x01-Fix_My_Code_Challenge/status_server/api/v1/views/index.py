d9ba6597cfcc21a5543731c9215ec4f6f42a3fa1
THEKINGSTAR <thekingstar5002@gmail.com>
2023-10-16 07:45:53 +0300

    0. Server status

--- 0x01-Fix_My_Code_Challenge/status_server/api/v1/views/index.py
+++ 0x01-Fix_My_Code_Challenge/status_server/api/v1/views/index.py
@@ -4,6 +4,10 @@
 from flask import jsonify
 
 from api.v1.views import app_views
+from flask import Blueprint, jsonify
+
+
+app_views = Blueprint("app_views", __name__)
 
 
 @app_views.route('/api/v1/status', methods=['GET'], strict_slashes=False)
