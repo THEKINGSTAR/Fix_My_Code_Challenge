068baf2ead5675839e01732fbac54bb9517842fd
THEKINGSTAR <thekingstar5002@gmail.com>
2023-10-17 03:55:27 +0300

    FIX DOCUMINTATINO

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
+"""
+Module containe one class to print info about square
+"""
+
 
 class square():
+    """
+    class to print info about the square
+    """
 
     width = 0
     height = 0
@@ -14,13 +21,16 @@ class square():
         return self.width * self.width
 
     def PermiterOfMySquare(self):
+        """ PERMITER of the square """
         return (self.width * self.height * 2)
 
     def __str__(self):
+        """ SQUARE STRING """
         return "{}/{}".format(self.width, self.height)
 
 
 if __name__ == "__main__":
+    """ MAIN FUNCTION CALL CHECK """
 
     s = square(width=12, height=9)
     print(s)
